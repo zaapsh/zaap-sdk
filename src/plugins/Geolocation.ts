d07079cd214ccb679572f1ef8e4520c039780c48
antoineaudrain <antoine.audrain@outlook.fr>
2021-04-11 11:59:25 +0200

    add Geolocation plugin

---
+++ src/plugins/Geolocation.ts
@@ -0,0 +1,26 @@
+export interface GeolocationOptions {
+  enableHighAccuracy?: boolean
+  timeout?: number
+  maximumAge?: number
+}
+
+export interface GeolocationPosition {
+  timestamp:  number
+  coords: {
+    latitude: number
+    longitude: number
+    accuracy: number
+    altitudeAccuracy?: number
+    altitude?: number
+    speed?: number
+    heading?: number
+  }
+}
+
+export default interface GeolocationPlugin {
+  getCurrentPosition(options?: GeolocationOptions): Promise<GeolocationPosition>
+
+  // watchPosition(options: GeolocationOptions, callback: GeolocationWatchCallback): string
+  //
+  // clearWatch(options: { id: string; }): Promise<void>
+}
--- src/plugins/Storage.ts
+++ src/plugins/Storage.ts
@@ -1,7 +1,11 @@
 export default interface StoragePlugin {
   get(key: string): Promise<{ value: string | null }>
+
   set(key: string, value: string): Promise<void>
+
   remove(key: string): Promise<void>
+
   clear(): Promise<void>
+
   keys(): Promise<{ keys: string[] }>
 }
